15838f18e95cf7e29d96d4b1f859ac32d52a8a5c
anna-masl <anna.maslinnikova@gmail.com>
2024-10-16 21:38:00 +0300

    fixed solution

--- src/app/SolutionHomeWork09.java
+++ src/app/SolutionHomeWork09.java
@@ -6,6 +6,17 @@ public class SolutionHomeWork09 {
     public static void main(String[] args) {
         int[][] matrix = new int[4][4];
         Random random = new Random();
+//        int [][] matrix = new int[][]{{8,1,6},{3,5,7},{4,9,2}};
+
+        int sumRowOdd = 0;
+        int sumRowEven = 0;
+        long prodColOdd = 1L;
+        long prodColEven = 1L;
+        boolean isMagicSquare = true;
+        int sumControl = 0; //сумма main diagonal
+        int sumDiagonal = 0;
+        int sumRow = 0;
+        int sumColumn = 0;
 
         System.out.println("Матриця 4x4:");
 
@@ -16,10 +27,6 @@ public class SolutionHomeWork09 {
             }
             System.out.println();
         }
-        int sumRowOdd = 0;
-        int sumRowEven = 0;
-        long prodColOdd = 1L;
-        long prodColEven = 1L;
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
@@ -36,61 +43,35 @@ public class SolutionHomeWork09 {
         System.out.println("Добуток елементів у парних стовпцях (стовпець 0, 2): " + prodColEven);
         System.out.println("Добуток елементів у непарних стовпцях (стовпець 1, 3): " + prodColOdd);
 
-        int result = 1;
-        int sum0 = 0; //сумма 1 строки
-        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            sumControl += matrix[i][i];
+            sumDiagonal += matrix[i][matrix.length - i - 1];
+        }
+//        System.out.println("sum of diag1 " + sumControl);
+//        System.out.println("sum of diag2 " + sumDiagonal);
 
-        for (int j = 0; j < matrix.length; j++) {
-            sum0 += matrix[0][j];
+        if (sumDiagonal != sumControl) {
+            isMagicSquare = false;
+            System.out.println("Матриця не є магічним квадратом.");
         }
-        for (int i = 0; i < matrix.length; i++) { //count sum for row
-            sum = 0;
-            for (int j = 0; j < matrix[i].length; j++) {
-                sum += matrix[i][j];
-            }
-//            System.out.println("sum of row" + i + " " + sum);
-            if (sum != sum0) {
-                result = 0;
-                System.out.println("Матриця не є магічним квадратом.");
-                break;
-            }
-        }
-        if (result == 1) {
-            for (int j = 0; j < matrix.length; j++) { //count sum for column
-                sum = 0;
-                for (int i = 0; i < matrix.length; i++) {
-                    sum += matrix[i][j];
+
+        if (isMagicSquare) {
+            for (int i = 0; i < matrix.length; i++) { //count sum for row
+                sumRow = 0;
+                sumColumn = 0;
+                for (int j = 0; j < matrix[i].length; j++) {
+                    sumRow += matrix[i][j];
+                    sumColumn += matrix[j][i];
                 }
-//                System.out.println("sum of colum" + j + " " + sum);
-                if (sum != sum0) {
-                    result = 0;
+//                System.out.println("sum of row" + i + " " + sumRow);
+//                System.out.println("sum of column" + i + " " + sumColumn);
+                if (sumRow != sumControl || sumColumn != sumControl) {
+                    isMagicSquare = false;
                     System.out.println("Матриця не є магічним квадратом.");
                     break;
                 }
             }
-        }
-        if (result == 1) {
-            sum = 0;
-            for (int i = 0; i < matrix.length; i++) {
-                sum += matrix[i][i];
-            }
-//            System.out.println("sum of diag1 " + sum);
-            if (sum != sum0) {
-                result = 0;
-                System.out.println("Матриця не є магічним квадратом.");
-            }
-
-        }
-        if (result == 1) {
-            sum = 0;
-            for (int i = 0; i < matrix.length; i++) {
-                sum += matrix[i][matrix.length - i - 1];
-            }
-//            System.out.println("sum of diag2 " + sum);
-            if (sum != sum0) {
-                result = 0;
-                System.out.println("Матриця не є магічним квадратом.");
-            } else System.out.println("Матриця є магічним квадратом.");
+            System.out.println("Матриця є магічним квадратом.");
         }
     }
 }
